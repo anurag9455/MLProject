ec635b73782783f2b12a7d95ba0d31f4c0ef2458
Anurag Srivastava <anuragsrivasta56789@gmail.com>
2023-03-18 03:59:04 +0530

    Data Transformation Completed

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -48,8 +48,8 @@ class DataTransformation:
             cat_pipeline= Pipeline(
                 steps=[
                 ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder",OneHotEncoder(sparse=False)),
-                ("scaler",StandardScaler())
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
                 ]
             )
             logging.info("Numerical columns standard scaling completed")
